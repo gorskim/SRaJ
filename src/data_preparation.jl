e6da20e4e432c600538d2dbcc876ade8045307c3
Mateusz Gorski <mateu.gorski@gmail.com>
2019-08-28 18:15:15 +0200

    Implement crop_image function

--- src/data_preparation.jl
+++ src/data_preparation.jl
@@ -1,8 +1,6 @@
-"""
-This file contains all the functions needed for data preparation.
-"""
+using Images, Colors
+using TestImages: testimage
 
-using Images, TestImages, Colors
 
 # constants definition
 TEST_IMAGES_DIR = "data/test_data"
@@ -11,29 +9,34 @@ TEST_IMAGES = ["lighthouse", "mandril_color", "lena_color_512", "peppers_color",
 
 
 """
-    crop_image(image, dims)
+    crop_image(image, new_size)
 
-Crop a smaller image out of an input one.
+Crop smaller images (square) out of an input one.
 
 # Examples
-crop_image(img, [1:64, 1:64])
+crop_image(img, 64)
 """
-function crop_image(image, dims::Array{UnitRange{Int64},1})
-    img = load(image_file)
-    img[dims]
-end
+function crop_image(image::Array, new_size::Int64)
+    # get rid of the residual pixels
+    new_height = height(image) - height(image) % new_size
+    new_width = width(image) - width(image) % new_size
+    cropped_size = min(new_height, new_width)
+    image = image[1:cropped_size, 1:cropped_size]
 
-
-"""
-    decrease_image(image, factor)
-
-Decrease an image by a given factor.
-"""
-function decrease_image(image, factor::Int)
-    for num in 1:(factor - 1)
-        image = restrict(image)
+    # create the array for new images
+    cropped_images = []
+    horizontal_start = 1
+    for i in 1:Int(cropped_size / new_size)
+        vertical_start = 1
+        for j in 1:Int(cropped_size / new_size)
+            cropped_img = image[vertical_start:(j*new_size),
+                                horizontal_start:(i*new_size)]
+            push!(cropped_images, cropped_img)
+            vertical_start += new_size
+        end
+        horizontal_start += new_size
     end
-    image
+    cropped_images
 end
 
 
@@ -50,21 +53,5 @@ function prepare_test_data(test_images::Vector{String}, dir::String)
     for image_name in test_images
         img = testimage(image_name)
         save("$dir/$image_name.png", img)
-    end
-end
-
-
-"""
-    prepare_data(dir)
-
-Prepare data (images) in a given directory for the training process.
-This is the main and only function to be used in training.jl
-"""
-function prepare_data(dir::String, decrease_factor::Int8,
-                      dims::Array{UnitRange{Int64},1})
-    for img_file in readdir(dir)
-        img = load(img_file)
-
-        # TODO
     end
 end
