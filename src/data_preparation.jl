442f6782f3bb6cbd552d2560fc2100e1cc4608c3
Mateusz Gorski <mateu.gorski@gmail.com>
2019-09-01 05:16:35 +0200

    Implement data preparation script

--- src/data_preparation.jl
+++ src/data_preparation.jl
@@ -1,11 +1,14 @@
-using Images, Colors
+using Images, Colors, JLD2, ProgressMeter
 using TestImages: testimage
 
 
 # constants definition
-TEST_IMAGES_DIR = "data/test_data"
+TRAINING_DATASET_DIR = "data/dataset"
+TEST_IMAGES_DIR = "data/test_data"  # testing = playing with new methods, etc.
 TEST_IMAGES = ["lighthouse", "mandril_color", "lena_color_512", "peppers_color",
                "woman_blonde"]
+IMAGE_ZOOM = 0.25
+IMAGE_SIZE = 64  # e.g. 64 stands for 64x64 pixels (always square images)
 
 
 """
@@ -24,7 +27,7 @@ function crop_image(image::Array, new_size::Int64)
     image = image[1:cropped_size, 1:cropped_size]
 
     # create the array for new images
-    cropped_images = []
+    cropped_images = Array[]
     horizontal_start = 1
     for i in 1:Int(cropped_size / new_size)
         vertical_start = 1
@@ -54,4 +57,49 @@ function prepare_test_data(test_images::Vector{String}, dir::String)
         img = testimage(image_name)
         save("$dir/$image_name.png", img)
     end
+end
+
+
+"""
+    prepare_data(dir, zoom, new_size)
+
+Prepare data (images) in a given directory for the training process.
+
+# Examples
+prepare_data("data/dataset", 0.25, 64)
+"""
+function prepare_data(dir::String, zoom::Float64, new_size::Int64)
+    # prepare the set of images of the default quality
+    x = Matrix{Float32}[]
+    y = Matrix{Float32}[]
+    @info "Cropping, resizing, creating matrices of arrays:"
+    @showprogress for img_file in readdir(dir)
+        img = load(joinpath(dir, img_file))
+        cropped_images = crop_image(RGB.(float.(img)), new_size)
+        for cropped in cropped_images
+            resized = imresize(cropped, ratio=zoom)
+            x = vcat(x, [resized])
+        end
+        y = vcat(y, cropped_images)
+    end
+
+    @info "Converting low-resolution data to 4D array (channelview)..."
+    @time lr = reduce((a, b) -> cat(a, b, dims=4), permutedims(channelview(img),
+               (2, 3, 1)) for img in x)
+    @info "Converting high-resolution data to 4D array (channelview):"
+    @time hr = reduce((a, b) -> cat(a, b, dims=4), permutedims(channelview(img),
+               (2, 3, 1)) for img in y)
+
+    lr, hr
+end
+
+
+function main()
+    # x, y = prepare_data(TEST_IMAGES_DIR, IMAGE_ZOOM, IMAGE_SIZE)
+    # @info "Saving to JLD2 format..."
+    # @save joinpath(TEST_IMAGES_DIR, "test_dataset.jld2") x y
+
+    lr, hr = prepare_data(TRAINING_DATASET_DIR, IMAGE_ZOOM, IMAGE_SIZE)
+    @info "Saving to JLD2 format..."
+    @save joinpath(TRAINING_DATASET_DIR, "training_dataset.jld2") lr hr
 end
