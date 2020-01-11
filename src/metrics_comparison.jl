5c4c140d5c8904a66a7d4fde4b761f42f713734a
Mateusz Gorski <mateu.gorski@gmail.com>
2020-01-11 13:29:20 +0100

    Add templates for needed methods

---
+++ src/metrics_comparison.jl
@@ -0,0 +1,3 @@
+using ImageQualityIndexes
+
+# calculate PSNR/SSIM
--- src/processing.jl
+++ src/processing.jl
@@ -1,30 +1,55 @@
-using Knet, Colors, Images, Statistics
+using Knet
 
 include("data_preparation.jl")
 
+global atype = gpu() >= 0 ? KnetArray{Float32} :  Array{Float32}
+const ε = Float32(1e-8)
+IMAGE_ZOOM = 0.25
+IMAGE_SIZE = 64  # e.g. 64 stands for 64x64 pixels (always square images)
+IMAGE_CHANNELS = 3
+EPOCHS = 10^4
+BATCH_SIZE = 100
+SEED = 123
+SAVE_FREQ = 100  # ? is it necessary?
+ALPHA = 0.2
+LEARNING_RATE = 0.0002  # for optimizer (Adam)
+BETA1 = 0.5  # Adam parametetrs for bias corrected moments
+NOISE_DIM = 100
 
-"""
-    prepare_data(dir, zoom, new_size)
+xtrn,ytrn,_,_= mnist()
 
-Prepare data (images) in a given directory for the training process.
+xtrn[:,:,1,1]
+xtrn
+dtrn = minibatch(lr, hr, 100; shuffle=true, xtype=Array{Float32})
 
-# Examples
-prepare_data("data/dataset", 0.25, 64)
-"""
-function prepare_data(dir::String, zoom::Float64, new_size::Int64)
-    # prepare the set of images of the default quality
-    Y = []
-    for img_file in readdir(dir)
-        img = load(img_file)
-        cropped_images = crop_image(img, new_size)
-        Y = vcat(Y, cropped_images)
-    end
+###### main
+wd, wg, md, mg = load_weights
 
-    # prepare the set of images of lower quality
-    X = []
-    for img in Y
-        resized = imresize(img, zoom)
-        X = vcat(X, resized)
-    end
-    X, Y
+function D()
+end
+
+function G()
+end
+
+function init_weights(atype=atype, noise_dim=NOISE_DIM, winit)
+    w = Any[]
+    m = Any[]
+end
+
+
+function read_weights()
+end
+
+
+function save_weights()
+end
+
+
+function modified_loss()
+end
+
+function train_discriminator!()
+end
+
+function train_generator!()
 end
